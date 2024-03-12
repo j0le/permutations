4d3461193225223576d06bc765a90835c9eeb9fd
Ole <j0le@users.noreply.github.com>
2024-03-12 22:49:24 +0100

    Print a binary permutation
    The number of lines is the binomal coefficient (n over k).

--- permutationen.cpp
+++ permutationen.cpp
@@ -5,14 +5,19 @@
 #include <utility>
 #include <cassert>
 
+
+static void print_span(std::span<char> s){
+    std::string_view view(s.data(), s.size());
+    std::print("{}\n",view);
+}
+
 static void print_permutation(std::span<char> all, std::span<char> filled, std::span<char> unfilled){
     std::size_t sum = filled.size() + unfilled.size();
     assert(std::cmp_equal(sum, all.size()));
     assert(filled.empty() || (all.begin() == filled.begin()));
     assert(unfilled.empty() || (all.begin()+filled.size())==unfilled.begin());
     if (std::cmp_equal(unfilled.size(), 0)) {
-        std::string_view view(filled.data(), filled.size());
-        std::print("{}\n",view);
+        print_span(filled);
         return;
     }
     char c = 'A';
@@ -53,7 +58,36 @@ bool print_permutation(std::uint32_t places){
     return true;
 }
 
+static void print_binary_permutation(std::span<char> all, std::span<char> rest, std::size_t part){
+    if (rest.empty()){
+        //if(!all.empty()){
+            print_span(all);
+        //}
+        return;
+    }
+    assert(std::cmp_greater_equal(rest.size(), part));
+    if (std::cmp_greater(part, 0z)){
+        rest[0] = 'x';
+        print_binary_permutation(all, rest.subspan(1z), part-1z);
+    }
+    if(std::cmp_greater(rest.size(), part)){
+        rest[0] = '_';
+        print_binary_permutation(all, rest.subspan(1z), part);
+    }
+}
+
+bool print_binary_permutation(std::uint32_t places, std::uint32_t part){
+    if(std::cmp_less(places, part)){
+        return false;
+    }
+    std::string str(places, '\0');
+    std::span<char> span(str.data(), str.length());
+    print_binary_permutation(span, span, part);
+    return true;
+}
+
 int main(){
-    print_permutation(3);
+    //print_permutation(4);
+    print_binary_permutation(10,5); // n over k, binomal coefficient
     return 0;
 }
