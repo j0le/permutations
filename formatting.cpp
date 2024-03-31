fc4489a4125293cd6b66f51c1b39ac05b92955ca
Ole <j0le@users.noreply.github.com>
2024-03-31 18:14:10 +0200

    Format formatting.cpp with clang-format

--- formatting.cpp
+++ formatting.cpp
@@ -5,51 +5,45 @@
 #include <iostream>
 #include <sstream>
 #include <string_view>
- 
-struct QuotableString : std::string_view
-{};
- 
-template<>
-struct std::formatter<QuotableString, char>
-{
+
+struct QuotableString : std::string_view {};
+
+template <> struct std::formatter<QuotableString, char> {
     bool quoted = false;
- 
-    template<class ParseContext>
-    constexpr ParseContext::iterator parse(ParseContext& ctx)
-    {
+
+    template <class ParseContext>
+    constexpr ParseContext::iterator parse(ParseContext &ctx) {
         auto it = ctx.begin();
         if (it == ctx.end())
             return it;
- 
-        if (*it == '#')
-        {
+
+        if (*it == '#') {
             quoted = true;
             ++it;
         }
         if (*it != '}')
             throw std::format_error("Invalid format args for QuotableString.");
- 
+
         return it;
     }
- 
-    template<class FmtContext>
-    FmtContext::iterator format(QuotableString s, FmtContext& ctx) const
-    {
+
+    template <class FmtContext>
+    FmtContext::iterator format(QuotableString s, FmtContext &ctx) const {
         std::ostringstream out;
         if (quoted)
             out << std::quoted(s);
         else
             out << s;
- 
+
         return std::ranges::copy(std::move(out).str(), ctx.out()).out;
     }
 };
- 
-int main()
-{
+
+int main() {
     QuotableString a("be"), a2(R"( " be " )");
     QuotableString b("a question");
     std::cout << std::format("To {0} or not to {0}, that is {1}.\n", a, b);
     std::cout << std::format("To {0:} or not to {0:}, that is {1:}.\n", a, b);
-    std::cout << std::format("To {0:#} or not to {0:#}, that is {1:#}.\n", a2, b);
+    std::cout << std::format("To {0:#} or not to {0:#}, that is {1:#}.\n", a2,
+                             b);
 }
